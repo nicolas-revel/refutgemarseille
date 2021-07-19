08553797dd121c446013faba02e6faae21a763f1
Nicolas Revel <nicolas.revel@laplateforme.io>
2021-07-19 22:21:12 +0200

    header done

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
-
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 // start the Stimulus application
 import './bootstrap';
